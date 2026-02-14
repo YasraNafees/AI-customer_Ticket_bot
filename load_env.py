fb93caff59cb6c182f46525de083b7b72ee9b0e4
Yasra Nafees <yasranafees419@gmail.com>
2026-02-14 08:35:54 +0500

    Refactor environment variable loading for OpenRouter

--- load_env.py
+++ load_env.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 
  # loading api key 
 load_dotenv()
-openai_api_key=os.getenv("OPENROUTER_API_KEY_1")
-openai_base_url=os.getenv("OPENROUTER_API_BASE")
+OPENROUTER_api=os.getenv("OPENROUTER_API_KEY_1")
+OPENROUTER_base_url=os.getenv("OPENROUTER_API_BASE")
 
-if not openai_api_key:
-    raise RuntimeError("openai api is not found .env")
+if not OPENROUTER_api:
+    raise RuntimeError("OPENROUTER_api is not found .env")
 
 VECTORSTORE_DIR="vectorstore"
 logger.info(f"Vectorstore directory set to :{VECTORSTORE_DIR}")
